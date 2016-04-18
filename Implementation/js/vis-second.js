cd951701dce440dcb6955c025fd905caea43e1dc
Anubha Srivastava <snowby98@Anubhas-MacBook-Air.local>
2016-04-18 18:45:34 -0400

    Vis second bar graph

--- Implementation/js/vis-second.js
+++ Implementation/js/vis-second.js
@@ -45,9 +45,85 @@ queue()
         });
 
         // console showing your data so you can build your stuff
-        //console.log(C02DataCsv);
+        console.log(C02DataCsv);
         //console.log(GDPDataCsv);
         //console.log(energyDataCsv);
 
-        // IMPLEMENT YOUR VISUALIZATIONS HERE
+        console.log(C02DataCsv[1][2011]);
+
+    var C02= [];
+    var j;
+    var i;
+  for (j=0; j< C02DataCsv.length; j++) {
+      for (i = 0; i < j; i++) {
+          C02 = C02DataCsv[i][2011]
+      }
+  }
+
+    console.log(C02);
+
+
+        //creating parameters
+        var margin = {top: 20, right: 20, bottom: 30, left: 40},
+            width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+        var x = d3.scale.ordinal()
+            .rangeRoundBands([0, width], .1);
+
+        var y = d3.scale.linear()
+            .range([height, 0]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom");
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(10, "%");
+
+        var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        console.log(C02DataCsv[0].Code);
+
+
+            x.domain(data.map(function(d) { return C02DataCsv[0].Code; }));
+            y.domain([0, d3.max(data, function(d) { return C02DataCsv[0][2013]; })]);
+
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
+
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .text("Frequency");
+
+            svg.selectAll(".bar")
+                .data(data)
+                .enter().append("rect")
+                .attr("class", "bar")
+                .attr("x", function(d) { return x(C02DataCsv[0].Code); })
+                .attr("width", x.rangeBand())
+                .attr("y", function(d) { return y(C02DataCsv[0][2013]); })
+                .attr("height", function(d) { return height - y(C02DataCsv[0][2013]); });
+
+
+
+
+        //derived from Mike Bostock's https://bl.ocks.org/mbostock/3943967 Tutorial
+
+
+
     });
